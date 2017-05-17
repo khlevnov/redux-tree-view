ea4c26f15a567fe0eed9c428c8974a847bf43af3
Oleg Khlevnov <oleg@khlevnov.ru>
2017-05-18 01:22:39 +0300

    Причесан внешний вид списка

--- src/containers/Node/Node.js
+++ src/containers/Node/Node.js
@@ -1,17 +1,12 @@
 import { connect } from 'react-redux';
 import Node from '../../components/Node/Node';
-import { addChild, remove } from '../../actions/actions';
+import * as actions from '../../actions/actions';
 
 const mapStateToProps = (state) => {
-  return {
-    state
-  };
+  return { state };
 };
 
 export default connect(
   mapStateToProps,
-  {
-    addChild,
-    remove
-  }
+  actions
 )(Node);
