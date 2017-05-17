f28e4418da65704a5ae92f197e3673f8e5c138b1
Oleg Khlevnov <oleg@khlevnov.ru>
2017-05-17 19:41:01 +0300

    Удалены неиспользуемые файлы

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import React, { Component } from 'react';
 import Node from './components/Node';
 
-class App extends Component {
-  render() {
-    return (
-      <ul>
-        <Node title="Element 1" isRoot>
-          <Node title="Element 2" />
-          <Node title="Element 3">
-            <Node title="Element 5" />
-            <Node title="Element 6" />
-          </Node>
-          <Node title="Element 4" />
-        </Node>
-      </ul>
-    );
-  }
+const App = () => {
+  <ul>
+    <Node title="Element 1" isRoot>
+      <Node title="Element 2" />
+      <Node title="Element 3">
+        <Node title="Element 5" />
+        <Node title="Element 6" />
+      </Node>
+      <Node title="Element 4" />
+    </Node>
+  </ul>
 }
 
 export default App;
